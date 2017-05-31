3ae854d1f0f4c68c8c611a4c06d221b7b337dd57
Carlos Orlando Castro <carlos-orlando.castro@atos.net>
2017-05-31 20:46:10 -0300

    swagger-ui

--- src/main/java/com/worldline/curso/Application.java
+++ src/main/java/com/worldline/curso/Application.java
@@ -14,17 +14,19 @@ import com.worldline.curso.dao.ServicioDao;
 import com.worldline.curso.entities.Poi;
 import com.worldline.curso.entities.Servicio;
 
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Tag;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableTransactionManagement
 @SpringBootApplication
+@EnableSwagger2
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
@@ -38,22 +40,22 @@ public class Application extends SpringBootServletInitializer {
 
 	
 
-//    @Bean
-//    public Docket api()
-//    {
-//        return new Docket(DocumentationType.SWAGGER_2).select()
-//            .apis(RequestHandlerSelectors.basePackage("com.worldline.curso.controller"))
-//            .paths(PathSelectors.any()).build().apiInfo(metadata()).tags(new Tag("Poi","Poi"));
-//    }
-//
-//    private ApiInfo metadata()
-//    {
-//        return new ApiInfoBuilder()
-//            .title("Share Data Manager API")
-//            .description(
-//                "Service that expose REST API to access to Poi data")
-//            .version("1.0.0-SNAPSHOT").build();
-//    }
+    @Bean
+    public Docket api()
+    {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors.basePackage("com.worldline.curso.controller"))
+            .paths(PathSelectors.any()).build().apiInfo(metadata()).tags(new Tag("Poi","Poi"));
+    }
+
+    private ApiInfo metadata()
+    {
+        return new ApiInfoBuilder()
+            .title("Poi Service API")
+            .description(
+                "Service that expose REST API to access to Poi data")
+            .version("1.0.0-SNAPSHOT").build();
+    }
 
 	
 	@Bean
--- src/test/java/com/worldline/curso/test/PoiIT.java
+++ src/test/java/com/worldline/curso/test/PoiIT.java
@@ -1,25 +1,19 @@
 package com.worldline.curso.test;
 
-import org.junit.Test;
-import org.springframework.web.client.RestTemplate;
-
-import com.worldline.curso.entities.PoisResponse;
-
-
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT,classes = Application.class)
 public class PoiIT {
-		
-	 private RestTemplate restTemplate=new RestTemplate();
-	 
-	 private String hostService="http://localhost:8080/v1/poiservice";
-
-	
-	@Test
-	public void exampleTest() {
-		PoisResponse resp=restTemplate.getForObject(hostService+"/findPoiByService/Buenos Aires Turistica",PoisResponse.class);
-		System.out.println(resp);
-	}
+//		
+//	 private RestTemplate restTemplate=new RestTemplate();
+//	 
+//	 private String hostService="http://localhost:8080/v1/poiservice";
+//
+//	
+//	@Test
+//	public void exampleTest() {
+//		PoisResponse resp=restTemplate.getForObject(hostService+"/findPoiByService/Buenos Aires Turistica",PoisResponse.class);
+//		System.out.println(resp);
+//	}
 	
 
 
